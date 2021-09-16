2cac7326e1d54b12fe9a218d947e03992144b675
Yikun Jiang <yikunkero@gmail.com>
2021-09-16 16:38:49 +0800

    Update download.sh

--- openeuler/download.sh
+++ openeuler/download.sh
@@ -3,7 +3,8 @@
 set -ex
 
 archs="x86_64 aarch64"
-versions="21.03"
+input_version=$1
+versions=${input_version:-"20.03-lts 20.03-lts-sp1 20.03-lts-sp2 20.09 21.03"}
 for ARCH in $archs ;
 do
     if [[ "$ARCH" = "aarch64" ]];then
